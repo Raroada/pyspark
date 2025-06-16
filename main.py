785dd9f2a53779b7e978eff6e383d3ca38b3238b
Raroada <167121938+Raroada@users.noreply.github.com>
2025-06-15 19:49:22 -0500

    local env setup

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.createDataFrame("test", "test")
+from pyspark.sql.types import StructField, StructType,StringType
+
+spark = (
+    SparkSession.builder
+        .master("local")
+        .appName("Local build")
+        .config("spark.some.config.option", "some-value")
+        .getOrCreate()
+)
+
+schema = StructType(
+            [
+                StructField('random_column', StringType())
+            ]
+        )
+
+df = spark.createDataFrame(
+    [(14, "Tom"), (23, "Alice"), (16, "Bob")], ["age", "name"])
+
+df.show()
